b721cc6694e3cd63db14ef0bf9ca5f94d1164d96
ivan.radojevic <ivandodo@gmail.com>
2017-01-10 13:53:17 +0100

    added requirements

--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,19 +1,27 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView, ListView, CreateView
+from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from .models import Tweet
 from .forms import TweetModelForm
+from .mixins import FormUserNeededMixin, UserOwnerMixin
 
 
 # Create your views here.
 
-class TweetCreateView(CreateView):
+#If you want to show form even to not authenticated user then you use FormUserNeededMixin
+class TweetCreateView(FormUserNeededMixin, CreateView):
     form_class = TweetModelForm
     template_name = 'tweets/create_view.html'
     success_url = "/tweets"
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super(TweetCreateView, self).form_valid(form)
+    #login_url="/admin/"
+
+class TweetUpdateView(LoginRequiredMixin, UserOwnerMixin, UpdateView):
+    queryset = Tweet.objects.all()
+    form_class = TweetModelForm
+    success_url = "/tweets"
+    template_name = 'tweets/update_view.html'
 
 class TweetDetailView(DetailView):
     template_name = "tweets/detail_view.html"
@@ -24,6 +32,13 @@ class TweetDetailView(DetailView):
         pk = self.kwargs.get("pk")
         obj = get_object_or_404(Tweet, pk=pk)
         return obj
+
+class TweetDeleteView(DeleteView):
+    #queryset = Tweet.object.all()
+    #model is the same as queryset
+    model = Tweet
+    template_name = "tweets/delete_confirm.html"
+    success_url = reverse_lazy("home")
 
 class TweetListView(ListView):
     template_name = "tweets/list_view.html"
