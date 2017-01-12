5c4aefa71848412f96a3e2ddcf9f6f262fb15795
ivan.radojevic <ivandodo@gmail.com>
2017-01-12 18:07:20 +0100

    41/67

--- src/tweets/api/serializers.py
+++ src/tweets/api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.utils.timesince import timesince
 
 from accounts.api.serializers import UserDisplaySerializer
 
@@ -6,10 +7,21 @@ from ..models import Tweet #from tweets.models import Tweet
 
 
 class TweetModelSerializer(serializers.ModelSerializer):
-    user = UserDisplaySerializer()
+    user = UserDisplaySerializer(read_only=True)
+    date_display = serializers.SerializerMethodField()
+    time_since = serializers.SerializerMethodField()
     class Meta:
         model = Tweet
         fields = [
             "user",
             "content",
+            "timestamp",
+            "date_display",
+            "time_since",
         ]
+
+    def get_date_display(self,obj):
+        return obj.timestamp.strftime("%b %d %I:%M %p")
+
+    def get_time_since(self,obj):
+        return timesince(obj.timestamp) + " ago"
--- src/tweets/api/urls.py
+++ src/tweets/api/urls.py
@@ -3,9 +3,11 @@ from django.conf.urls import url
 
 
 from .views import (
-    TweetListAPIView
+    TweetListAPIView,
+    TweetCreateAPIView,
 )
 
 urlpatterns = [
     url(r'^$', TweetListAPIView.as_view(), name ='list'),
+    url(r'^create/$', TweetCreateAPIView.as_view(), name ='create'),
 ]
