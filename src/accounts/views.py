f6eb3976398b5b5103a1a7bafc583a5cafe3dda0
ivan.radojevic <ivandodo@gmail.com>
2017-01-31 17:48:58 +0100

    41/67

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,6 +1,9 @@
 from django.views.generic import DetailView
+from django.views import View
 from django.contrib.auth import get_user_model
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .models import UserProfile
 
 # Create your views here.
 
@@ -12,3 +15,15 @@ class UserDetailView(DetailView):
 
     def get_object(self):
         return get_object_or_404(User, username__iexact=self.kwargs.get("username"))
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(UserDetailView, self).get_context_data(*args, **kwargs)
+        context['following'] = UserProfile.objects.is_following(self.request.user, self.get_object())
+        return context
+
+class UserFollowView(View):
+    def get(self, request, username, *args, **kwargs):
+        toggle_user = get_object_or_404(User, username__iexact=username)
+        if (request.user.is_authenticated()):
+            UserProfile.objects.toggle_follow(request.user, toggle_user)
+        return redirect("profiles:detail", username=username)
